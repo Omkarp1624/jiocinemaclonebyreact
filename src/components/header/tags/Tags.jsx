b93f62cd81534116858a9ca82f39ea597dac5e83
Patil Omkar <omkarp1624@gmail.com>
2025-04-13 11:41:48 +0530

    adding channels

--- src/components/header/tags/Tags.jsx
+++ src/components/header/tags/Tags.jsx
@@ -1,8 +1,33 @@
-import styles from './Tags.module.css'
+import React, {useState} from 'react';
+import styles from './tags.module.css'
 const Tags = () => {
+
+    let [tags,setTags] = useState([
+        "For You",
+        "Action",
+        "Drama",
+        "Sci-Fi",
+        "Thriller",
+        "Romantic Comedy",
+        "Superhero",
+        "Crime Series",
+        "Fantasy",
+        "Animated",
+        "Documentary",
+        "Mystery",
+        "Biopic",
+        "Family"
+    ])
     return (
         <>
-             <div className={styles.tags}/>
+             <div className={styles.tags}>
+                {
+                    tags.map((tag)=>{
+                        return <p className={styles.tag}>{tag}</p>
+                    })
+                }
+             </div>
+
         </>
     )
 }
